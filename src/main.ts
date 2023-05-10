01911699abc638b83d89dd26b783b161804485e6
emanuelhardwell <emanuelnoe16@gmail.com>
2023-05-09 23:40:12 -0600

    feat: Example the types was created

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import {
+  nombre,
+  apellido,
+  trabajando,
+  vacacion,
+  edad,
+  cantidad,
+} from "./bases/01-types";
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1> Hello vite </h1>
+    <p> ${nombre} ${apellido} </p>
+    <p> Esta trabajando: ${trabajando} </p>
+    <p> Esta de vacaciones: ${vacacion} </p>
+    <p> Edad actual: ${edad} </p>
+    <p> Cantidad a pagar: $${cantidad} </p>
   </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+`;
